4c5e9c4e0ffed5bb225078a943af9cb2d987848b
youknowlx0-hash <youknowlx0@gmail.com>
2026-09-10 20:33:36 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+BOT_TOKEN = os.getenv("8934627682:AAGUBPDZ3RIuDV_RkWitHy5JbE0J9WBammw")
+ADMIN_ID = int(os.getenv("7702942505", "0"))
+
+FORCE_CHANNELS = [
+    "@VenomXTricks",
+    "@VenomTricksOfficial",
+    "@TheProfitLoots",
+]
